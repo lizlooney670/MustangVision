5225d2f3af0f6eb3a91898b68f4d7ef6cd4c201a
Vikram Sharma <viksurf@gmail.com>
2016-11-10 19:16:10 -0800

    Update test.java

--- OPENCV_INSTALLATION/test.java
+++ OPENCV_INSTALLATION/test.java
@@ -1,1 +1,12 @@
+import org.opencv.core.Core;
 
+public class test {
+
+	public static void main(String[] args)
+	{
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        System.out.println("This should have run without errors");
+        System.out.println("If there were errors, install a different architecture file");
+	}
+	
+}
